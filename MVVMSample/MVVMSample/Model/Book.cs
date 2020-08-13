4038c096b8309e42462349757bd38c17a923aff0
dimaematveev <dimaematveev@gmail.com>
2020-08-13 21:18:01 +0300

    Удалил ненужную кнопку

---
+++ MVVMSample/MVVMSample/Model/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample.Model
+{
+    public class Book : INotifyPropertyChanged
+    {
+        string autor;
+        string title;
+        public string Autor
+        {
+            get { return autor; }
+            set
+            {
+                autor = value;
+                OnPropertyChanged(nameof(Autor));
+            }
+        }
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+        public static Book[] GetBooks()
+        {
+            var result = new Book[]
+            {
+                new Book(){Autor = "Лев Толстой", Title="Война и мир"},
+                new Book(){Autor = "Михаил Булгаков", Title="Мастер и Маргарита"},
+                new Book(){Autor = "Стивен Кинг", Title="Оно"}
+            };
+            return result;  
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+    }
+}
---
+++ MVVMSample/MVVMSample/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using MVVMSample.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample.ViewModel
+{
+    public class MainWindowViewModel :INotifyPropertyChanged
+    {
+        private Book[] books;
+        public Book[] Books 
+        {
+            get { return books; }
+            private set
+            {
+                books = value;
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            Books = Book.GetBooks();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
